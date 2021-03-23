d9d57a21fc81007b776b35860bb6567338c3f049
TASHENG <TASHENG@TASHENG-T490.cn.oracle.com>
2021-03-23 15:37:54 +0800

    'update'

--- src/main/java/com/oracle/tao/kafka/KafkaConsumer.java
+++ src/main/java/com/oracle/tao/kafka/KafkaConsumer.java
@@ -1,11 +1,15 @@
 package com.oracle.tao.kafka;
 
 import com.oracle.tao.service.VideoJsonService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class KafkaConsumer {
@@ -19,12 +23,38 @@ public class KafkaConsumer {
 
 
 
-    @KafkaListener(topics = "${spring.kafka.template.default-topic}")
-    public void receive(String message) throws SQLException {
+//    @KafkaListener(topics = "${spring.kafka.template.default-topic}")
+//    public void receive(String message) throws SQLException {
+//
+//        //this.conn= OracleDao.getConnection();
+//        //oracleDao.insertJSON(conn,message);
+//        videoJsonService.create(message);
+//        System.out.println("app_log--消费消息:" + message);
+//    }
 
-        //this.conn= OracleDao.getConnection();
-        //oracleDao.insertJSON(conn,message);
-        videoJsonService.create(message);
-        System.out.println("app_log--消费消息:" + message);
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}")
+    public void myListener(List<ConsumerRecord<Integer, String>> list){
+        System.out.print(list.size()+",");
+         list.forEach( it -> {
+             videoJsonService.create(it.value());
+        });
     }
+
+
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}", id = "consumer", containerFactory = "batchFactory")
+    public void listen(List<ConsumerRecord<?, ?>> list) {
+        List<String> messages = new ArrayList<>();
+        for (ConsumerRecord<?, ?> record : list) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            // 获取消息
+            kafkaMessage.ifPresent(o -> messages.add(o.toString()));
+            System.out.println(kafkaMessage);
+        }
+//        if (messages.size() > 0) {
+//            // 更新索引
+//            updateES(messages);
+//        }
+    }
+
+
 }
--- src/main/java/com/oracle/tao/kafka/KafkaProducerConfig.java
+++ src/main/java/com/oracle/tao/kafka/KafkaProducerConfig.java
@@ -3,16 +3,20 @@ package com.oracle.tao.kafka;
 
 
 
+
 import org.apache.kafka.clients.producer.ProducerConfig;
+
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,9 +46,7 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<String, String>(properties);
     }
 
+    }
 
-
-
-}
 
 
